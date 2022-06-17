141c905afdba3c96b1885046343b4c0b4593ac13
Phantom <1o1aqib@gmail.com>
2022-06-18 03:08:35 +0600

    added documentation comments to gradientbox.dart

--- lib/src/gradientbox.dart
+++ lib/src/gradientbox.dart
@@ -3,28 +3,64 @@ import 'package:flutter/material.dart';
 import '../gradientbox.dart';
 
 class GradientBox extends StatelessWidget {
+  /// If non-null, then defines what will happen on tapping the widget.
   final VoidCallback? onTap;
 
+  /// The [child] contained by the Gradient Box.
+  ///
+  /// If null, a `Text("Hello World")` widget will be added automatically
+  /// and Gradient Box will expand to fill all available space in its parent.
+  ///
+  /// If the parent provides unbounded constraints, in which case the box
+  /// will attempt to be as small as possible.
+  ///
+  /// This widget can only have one child. To lay out multiple children, let
+  /// this widget's child be a widget such as [Row], [Column], or [Stack],
+  /// which have a children property. and then provide the children to
+  /// that widget.
   final Widget? child;
 
   final double? height;
 
   final double? width;
 
+  /// If non-zero, the corners of this widget are rounder by the provide value.
+  ///
+  /// If null, then the default value is 25.
   final double? borderRadius;
 
+  /// Creates a Linear gradient.
+  ///
+  /// If non-null, the [colors] argument must be passed in `GradientColors()` .
+  ///
+  /// If null, then random gradient colors are chosen.
   final GradientColors? gradientColors;
 
   final bool showShadow;
 
+  /// Adds a shadow to the widget. [showShadow] must to true to see the effect.
+  ///
+  /// If null, then the default color is `colors.grey`.
   final Color? shadowColor;
 
+  /// Align the [child] within the box.
+  ///
+  /// If non-null, the Gradient Box will expand to fill its parent and position its
+  /// child within itself according to the given value. If the incoming
+  /// constraints are unbounded, then the child will be shrink-wrapped instead.
+  ///
+  /// If null, then will have a default value of `Alignment.center`.
+  ///
+  /// Ignored if [child] is null.
   final AlignmentGeometry? alignment;
 
+  /// Empty space surrounding the [child].
   final EdgeInsets? padding;
 
+  /// Empty space surrounding the widget.
   final EdgeInsets? margin;
 
+  ///Create a customizable Gradient Box widget with a beautiful gradient.
   const GradientBox(
       {Key? key,
       this.onTap,
